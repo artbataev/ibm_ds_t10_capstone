834ee58c674c9846b6bf1f6d5de76703f53b49cb
artbataev <artbataev@gmail.com>
2023-05-02 13:33:03 +0400

    Lab 3.2: Build a Dashboard Application with Plotly Dash - Done

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import dash
 import dash_html_components as html
-import dash_core_components as dcc
+from dash import dcc
 from dash.dependencies import Input, Output
 import plotly.express as px
 
@@ -23,7 +23,19 @@ app.layout = html.Div(
         ),
         # TASK 1: Add a dropdown list to enable Launch Site selection
         # The default select value is for ALL sites
-        # dcc.Dropdown(id='site-dropdown',...)
+        dcc.Dropdown(
+            id="site-dropdown",
+            options=[
+                {"label": "All Sites", "value": "ALL"},
+                *[
+                    {"label": launch_site, "value": launch_site}
+                    for launch_site in spacex_df["Launch Site"].unique()
+                ],
+            ],
+            value="ALL",
+            placeholder="Select a Launch Site here",
+            searchable=True,
+        ),
         html.Br(),
         # TASK 2: Add a pie chart to show the total successful launches count for all sites
         # If a specific launch site was selected, show the Success vs. Failed counts for the site
@@ -31,17 +43,84 @@ app.layout = html.Div(
         html.Br(),
         html.P("Payload range (Kg):"),
         # TASK 3: Add a slider to select payload range
-        # dcc.RangeSlider(id='payload-slider',...)
+        dcc.RangeSlider(
+            id="payload-slider",
+            min=0,
+            max=10_000,
+            step=1000,
+            value=[min_payload, max_payload],
+        ),
         # TASK 4: Add a scatter chart to show the correlation between payload and launch success
         html.Div(dcc.Graph(id="success-payload-scatter-chart")),
     ]
 )
 
+
 # TASK 2:
 # Add a callback function for `site-dropdown` as input, `success-pie-chart` as output
+@app.callback(
+    Output(component_id="success-pie-chart", component_property="figure"),
+    Input(component_id="site-dropdown", component_property="value"),
+)
+def get_pie_chart(entered_site):
+    if entered_site == "ALL":
+        fig = px.pie(
+            spacex_df,
+            values="class",
+            names="Launch Site",
+            title="Total Success Launches By Site",
+        )
+        return fig
+    else:
+        filtered_df = (
+            spacex_df[spacex_df["Launch Site"] == entered_site]
+            .groupby("class")
+            .size()
+            .reset_index(name="Number of launches")
+        )
+        # return the outcomes piechart for a selected site
+        fig = px.pie(
+            filtered_df,
+            values="Number of launches",
+            names="class",
+            title=f"Total Success Launches for site {entered_site}",
+        )
+        return fig
+
 
 # TASK 4:
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
+@app.callback(
+    Output(component_id="success-payload-scatter-chart", component_property="figure"),
+    [
+        [
+            Input(component_id="site-dropdown", component_property="value"),
+            Input(component_id="payload-slider", component_property="value"),
+        ],
+    ],
+)
+def get_success_payload_scatter_plot(entered_site, payload_range):
+    payload_range_min, payload_range_max = payload_range
+    # filter by launch site
+    if entered_site == "ALL":
+        filtered_df = spacex_df
+        title = "Correlation between Payload and Success for all Sites"
+    else:
+        filtered_df = spacex_df[spacex_df["Launch Site"] == entered_site]
+        title = f"Correlation between Payload and Success for site {entered_site}"
+    # filter by payload range
+    filtered_df = filtered_df[
+        (payload_range_min <= filtered_df["Payload Mass (kg)"])
+        & (filtered_df["Payload Mass (kg)"] <= payload_range_max)
+    ]
+    fig = px.scatter(
+        filtered_df,
+        x="Payload Mass (kg)",
+        y="class",
+        color="Booster Version Category",
+        title=title,
+    )
+    return fig
 
 
 # Run the app
